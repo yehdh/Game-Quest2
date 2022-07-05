d61eec48f0dcf4e84df6433f14c26848433cf577
unknown <ep6324@gmail.com>
2022-07-05 14:22:00 +0300

    Console app

---
+++ ConsoleApp1/ConsoleApp1/Items.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Item // предметы : Деньги, Молоко, Пиво
+	{
+		public string name;
+		public Item(string name)
+		{
+			this.name = name;
+		}
+	}
+}
